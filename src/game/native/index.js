ad199980e16ec73b31564595d648950205f99b97
Cody Morton <32003959+kxirk@users.noreply.github.com>
2024-05-14 20:29:02 -0600

    generate rename

--- src/game/native/index.js
+++ src/game/native/index.js
@@ -2,8 +2,8 @@ export { default as Empty } from "./tile/empty.js";
 export { default as Floor } from "./tile/floor.js";
 export { default as Wall } from "./tile/wall.js";
 
-export * from "./generate/depth.js";
-export * from "./generate/fov.js";
-export * from "./generate/mob.js";
-export * from "./generate/point.js";
-export * from "./generate/tile.js";
+export * from "./world/depth.js";
+export * from "./world/fov.js";
+export * from "./world/mob.js";
+export * from "./world/point.js";
+export * from "./world/tile.js";
